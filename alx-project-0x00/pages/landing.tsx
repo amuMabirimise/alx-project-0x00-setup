1df63069369a4cc8ded9e897108abba74502b9a5
amuMabirimise <awmabirimise@gmail.com>
2024-12-22 14:01:44 +0200

    Advanced Task

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
@@ -8,6 +9,20 @@ const Landing: React.FC = () => {
       <Card />
       <Card />
       <Card />
+      <div className="flex flex-wrap gap-4 mt-4">
+        <Button
+          title="Small Rounded"
+          styles="text-sm rounded-sm bg-blue-500 text-white"
+        />
+        <Button
+          title="Medium Rounded"
+          styles="text-base rounded-md bg-green-500 text-white"
+        />
+        <Button
+          title="Large Rounded"
+          styles="text-lg rounded-full bg-red-500 text-white"
+        />
+      </div>
     </div>
   );
 };
