b559fab7a8a54f2f52a32d3bc5e8258906016539
amuMabirimise <awmabirimise@gmail.com>
2025-03-25 12:30:14 +0200

    Rendering Components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,1 +1,15 @@
-// PILL.tsx
+import React from "react";
+
+interface PillProps {
+  title: string;
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="bg-[#E8F5E9] text-[#388E3C] rounded-full px-4 py-1 text-sm font-medium">
+      {title}
+    </div>
+  );
+};
+
+export default Pill;
